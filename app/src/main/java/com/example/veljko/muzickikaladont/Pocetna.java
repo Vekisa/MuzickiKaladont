b44964a1bb07831712ab06c93482273e832ad49f
Vekisa <vmosorinski@gmail.com>
2018-11-02 00:00:45 +0100

    Initial commit

---
+++ app/src/main/java/com/example/veljko/muzickikaladont/Pocetna.java
@@ -0,0 +1,26 @@
+package com.example.veljko.muzickikaladont;
+
+import android.annotation.SuppressLint;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.Window;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class Pocetna extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_pocetna);
+    }
+}
